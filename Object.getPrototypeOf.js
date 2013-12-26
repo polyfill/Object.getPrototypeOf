4dac0bfedc34ed588ad3de789157108366f7346d
Shallker <imshallker@gmail.com>
2013-12-26 16:39:09 +0800

    First commit

---
+++ Object.getPrototypeOf.js
@@ -0,0 +1,15 @@
+/**
+ * Polyfill for Object.getPrototypeOf
+ */
+if ( typeof Object.getPrototypeOf !== "function" ) {
+  if ( typeof "test".__proto__ === "object" ) {
+    Object.getPrototypeOf = function(object){
+      return object.__proto__;
+    };
+  } else {
+    Object.getPrototypeOf = function(object){
+      // May break if the constructor has been tampered with
+      return object.constructor.prototype;
+    };
+  }
+}
---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# Object.getPrototypeOf
+
+  Polyfill for Object.getPrototypeOf
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install polyfill/Object.getPrototypeOf
+
+## API
+
+
+
+## License
+
+  MIT
